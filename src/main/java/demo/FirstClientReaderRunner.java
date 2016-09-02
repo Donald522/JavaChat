51952f332274de7f2a65ad5d8340ef5e80086b39
antontolkachev <anton-t@inbox.ru>
2016-09-02 11:25:33 +0300

    Handle exceptions in demo

--- src/main/java/demo/FirstClientReaderRunner.java
+++ src/main/java/demo/FirstClientReaderRunner.java
@@ -1,13 +1,19 @@
 package demo;
 
 import client.ClientReader;
+import exceptions.ClientException;
 
 /**
  * Created by anton on 01.09.16.
  */
 public class FirstClientReaderRunner {
-    public static void main(String[] args) {
-        ClientReader clientReader = new ClientReader(1112);
+    public static void main(String[] args) throws ClientException {
+        ClientReader clientReader = null;
+        try {
+            clientReader = new ClientReader(1112);
+        } catch (ClientException e) {
+            throw new ClientException("Can't connect to the server", e);
+        }
         clientReader.run();
     }
 }
--- src/main/java/demo/SecondClientReaderRunner.java
+++ src/main/java/demo/SecondClientReaderRunner.java
@@ -1,13 +1,19 @@
 package demo;
 
 import client.ClientReader;
+import exceptions.ClientException;
 
 /**
  * Created by anton on 01.09.16.
  */
 public class SecondClientReaderRunner {
-    public static void main(String[] args) {
-        ClientReader clientReader = new ClientReader(1113);
+    public static void main(String[] args) throws ClientException {
+        ClientReader clientReader = null;
+        try {
+            clientReader = new ClientReader(1113);
+        } catch (ClientException e) {
+            throw new ClientException("Can't connect to the server", e);
+        }
         clientReader.run();
     }
 }
