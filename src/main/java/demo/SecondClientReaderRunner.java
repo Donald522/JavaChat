0bce662ebac06a9dad32ced8c0e50309a55e893f
antontolkachev <anton-t@inbox.ru>
2016-09-02 11:29:39 +0300

    bug fixed

--- src/main/java/demo/SecondClientReaderRunner.java
+++ src/main/java/demo/SecondClientReaderRunner.java
@@ -3,17 +3,19 @@ package demo;
 import client.ClientReader;
 import exceptions.ClientException;
 
+import static com.sun.xml.internal.ws.spi.db.BindingContextFactory.LOGGER;
+
 /**
  * Created by anton on 01.09.16.
  */
 public class SecondClientReaderRunner {
-    public static void main(String[] args) throws ClientException {
+    public static void main(String[] args) {
         ClientReader clientReader = null;
         try {
             clientReader = new ClientReader(1113);
+            clientReader.run();
         } catch (ClientException e) {
-            throw new ClientException("Can't connect to the server", e);
+            LOGGER.info("Can't connect to the server");
         }
-        clientReader.run();
     }
 }
